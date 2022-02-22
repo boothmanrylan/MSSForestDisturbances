42fa85e268cdca668aa8674c0d0a08796a2f7f03
Rylan Boothman <boothmanrylan@gmail.com>
2022-02-21 19:57:15 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='MSSForestDisturbances',
+    version='0.0.0',
+    author='Rylan Boothman',
+    author_email='boothmanrylan@gmail.com',
+    packages=['MSSForestDisturbances'],
+    install_requires=['earthengine-api', 'IPython', 'folium']
+)
