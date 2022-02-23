7488e2334c0362b69dde82a28647aa20a9556bc8
Rylan Boothman <boothmanrylan@gmail.com>
2022-02-23 09:37:33 -0500

    add rainbow palette

--- mfd/utils.py
+++ mfd/utils.py
@@ -8,6 +8,17 @@ CLASS_VIS = {
     'palette': ['grey', 'saddleBrown', 'red', 'blue', 'black', 'white']
 }
 
+RAINBOW_PALETTE = {
+    'A0A0A0', 'FF0000', 'FF1700', 'FF2F00', 'FF4700', 'FF5E00',
+    'FF7600', 'FF8E00', 'FFA600', 'FFBD00', 'FFD500', 'FFED00',
+    'F9FF00', 'E1FF00', 'C9FF00', 'B1FF00', '9AFF00', '82FF00',
+    '6AFF00', '53FF00', '3BFF00', '23FF00', '0BFF00', '00FF0B',
+    '00FF23', '00FF3B', '00FF53', '00FF6A', '00FF82', '00FF9A',
+    '00FFB1', '00FFC9', '00FFE1', '00FFF9', '00EDFF', '00D5FF',
+    '00BDFF', '00A6FF', '008EFF', '0076FF', '005EFF', '0047FF',
+    '002FFF', '0017FF', '0000FF',
+}
+
 
 def copyproperties(func):
     """Decorator to ensure func doesn't alter image metadata.
